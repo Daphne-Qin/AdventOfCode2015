a5c8f3ad66d79b2b8c40b1517fcd66f7534f3af3
Daphne Qin <dqin30@stuy.edu>
2021-11-24 11:35:50 -0500

    Moved input to txt file, + args for filename and mode.

---
+++ Problem1/Problem1.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem1 {
+  public static int findFloor(String dir) {
+    int floor = 0;
+    for (int i = 0; i < dir.length(); i++) {
+      if (dir.charAt(i) == '(') {
+        floor++;
+      } else {
+        floor--;
+      }
+    }
+    return floor;
+  }
+
+  public static int findBasement(String dir) {
+    int floor = 0;
+    for (int i = 0; i < dir.length(); i++) {
+      if (dir.charAt(i) == '(') {
+        floor++;
+      } else {
+        floor--;
+        if (floor < 0) {
+          return i+1;
+        }
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    String dir = "";
+
+    try {
+      File file = new File(args[0]);
+      Scanner input = new Scanner(file);
+      dir = input.nextLine();
+      input.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println("File not found!");
+    }
+
+    try {
+      if (Integer.parseInt(args[1]) == 1) System.out.println(findFloor(dir));
+      if (Integer.parseInt(args[1]) == 2) System.out.println(findBasement(dir));
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Invalid syntax: use \"java Problem1 [filename] [1/2]\"");
+    }
+  }
+}
