06dee8bc353678ebd0db2556ce15193b30e686a3
Daphne Qin <dqin30@stuy.edu>
2021-11-27 08:57:06 -0500

    Finished Problem 10. (Very inefficient part 2 - 72m35.891s!)

---
+++ Problem10/Problem10.java
@@ -0,0 +1,54 @@
+public class Problem10 {
+  //================================================================================
+  // TESTER
+  //================================================================================
+  public static void main(String[] args) {
+    String input = "1113222113";
+    boolean progressCheck = true;
+
+    try {
+      int repeat;
+      if (Integer.parseInt(args[0]) == 1) { // part 1
+        repeat = 40;
+      } else if (Integer.parseInt(args[0]) == 2) { // part 2
+        repeat = 50;
+      } else {
+        repeat = 0;
+      }
+
+      System.out.println("This might take a while!");
+      String result = input;
+      for (int i = 1; i <= repeat; i++) {
+        result = lookAndSay(result);
+        if (progressCheck) System.out.println(i + ": " + result.length());
+      }
+      System.out.println("Result: " + result.length());
+    } catch (Exception e) {
+      System.out.println("Invalid syntax: use \"java Problem10 [1/2]\".\nEdit the input inside the code.");
+    }
+  }
+
+  //================================================================================
+  // LOOK-AND-SAY MECHANISM
+  //================================================================================
+  private static String lookAndSay(String num) {
+    char current = num.charAt(0);
+    int count = 0;
+    String newNum = "";
+
+    for (int i = 0; i <= num.length(); i++) {
+      if (i != num.length() && current == num.charAt(i)) { // count same number of characters in a row, check that it's not the last character
+        count++;
+      } else { // end at different number, edit newNum, start the count over with the new number
+        newNum += count;
+        newNum += current;
+        if (i != num.length()) { // use new character if not at end of string
+          current = num.charAt(i);
+          count = 1; // not 0 since that would undercount by 1
+        }
+      }
+    }
+
+    return newNum;
+  }
+}
