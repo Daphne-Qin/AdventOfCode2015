ec32b96fb98a921a027222f6a92cc95ace16acfa
Daphne Qin <dqin30@stuy.edu>
2021-11-30 18:31:12 -0500

    Finished Problem 17.

--- Problem17/Problem17.java
+++ Problem17/Problem17.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Problem17 {
@@ -9,6 +10,8 @@ public class Problem17 {
   //================================================================================
   public static void main(String[] args) {
     ArrayList<Integer> containers = new ArrayList<Integer>();
+    int totalContainers;
+    int desiredSum = 150;
 
     try {
       File file = new File(args[0]);
@@ -20,40 +23,61 @@ public class Problem17 {
     } catch (FileNotFoundException ex) {
       System.out.println("File not found!");
     }
+    totalContainers = containers.size();
 
-    System.out.println(containers);
-
-    for (int i = 0; i < 1048576; i++) {
-      System.out.println(toBinary(i));
-    }
-
-/*
     try {
       String mode = args[1];
 
+      int result = 0;
+      ArrayList<Integer> numContainers = new ArrayList<Integer>(); // used for part 2
+
+      // goes through each iteration
+      for (int i = 0; i < Math.pow(2, totalContainers); i++) {
+        String on = fillBinary(Integer.toBinaryString(i), totalContainers); // 1s are added, 0s are not
+        if (selectiveSum(containers, on) == desiredSum) {
+          if (mode.equals("1")) {
+            result++;
+          } else if (mode.equals("2")) {
+            numContainers.add(Integer.bitCount(i)); // bitCount() is used for counting how many containers are used
+          }
+        }
+      }
+
+      // extra part 2 steps to find the lowest number of containers needed and how many permutations of that there are
+      if (mode.equals("2")) {
+        Collections.sort(numContainers);
+        int leastContainers = numContainers.get(0);
+        for (int i = 0; numContainers.get(i) == leastContainers; i++) {
+          result++;
+        }
+      }
+
+      if (!mode.equals("1") && !mode.equals("2")) result = -1;
+      System.out.println(result);
 
     } catch (ArrayIndexOutOfBoundsException e) {
-      System.out.println("Invalid syntax: use \"java Problem15 [filename] [1/2]\".");
+      System.out.println("Invalid syntax: use \"java Problem17 [filename] [1/2]\".");
     }
-*/
   }
 
-  private static int toBinary(int num) {
-    String binary = "";
-
-    int dividend = num;
-    int remainder;
-
-    while (dividend > 1) {
-      // add to the number first
-      remainder = num % 2;
-      binary = ""+remainder + binary;
-
-      // setup for next loop
-      dividend /= 2;
+  //================================================================================
+  // HELPER FUNCTIONS
+  //================================================================================
+  // adds 0s to the binary string until the string reaches length len
+  private static String fillBinary(String bin, int len) {
+    while (bin.length() < len) {
+      bin = "0" + bin;
     }
-    binary = ""+dividend + binary;
-
-    return Integer.parseInt(binary);
+    return bin;
+  }
+  // adds numbers based on binary - 0 means ignore, 1 means add (nums and on are the same length)
+  private static int selectiveSum(ArrayList<Integer> nums, String on) {
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (on.charAt(i) == '1') {
+        sum += nums.get(i);
+      }
+    }
+    return sum;
   }
 }
